175c0e4aee062da820680e58230d305379bc6e42
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-06-19 20:22:37 +0530

    fetching blocks completed

--- src/App.js
+++ src/App.js
@@ -1,106 +1,126 @@
 // import React from 'react'
 import React, { useState, useEffect } from 'react';
 export default function App() {
-  const [blocks,setBlocks] = useState([]);
+  var [blocks, setBlocks] = useState([]);
   let staticBlockCount = 5;
   useEffect(() => {
     // Update the document title using the browser API
-   
-    console.log("Called initially");
+
     getLatestBlock();
-  },[]);
+  }, []);
 
- async function getLatestBlock(){
-console.log("Getting latest block");
-const requestOptions = {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1})
-};
-fetch('https://ethblockchain.mohammedismayi1.repl.co/', requestOptions)
-  .then(response => response.json())
-  .then(data => 
-   
-   
-    console.log(data["result"]["number"]),
-   
-    // fe = await getLastFewBlocks(),
-    getLastFewBlocks().then(response => console.log(response))
-  );
+  async function getLatestBlock() {
+    // console.log("Getting latest block");
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "eth_getBlockByNumber",
+        params: ["latest", false],
+        id: 1,
+      }),
+    };
+    fetch("https://ismayilethtestnet.tk/", requestOptions)
+      .then((response) => response.json())
+      .then((data) =>
+        // fe = await getLastFewBlocks(),
+        getLastFewBlocks(data["result"]["number"])
+      );
 
-  
+    // const products = ["brush","keys","rings","watches"]
+    // setBlocks(products);
+  }
 
-
-  const products = ["brush","keys","rings","watches"]
-  setBlocks(products);
-  };
-
-
-  async function getLastFewBlocks(){
-
-
+  async function getLastFewBlocks(hexString) {
+    const latestblockNumber = parseInt(hexString, 16);
+    var emptyblocks = [];
     for (let index = 0; index < staticBlockCount; index++) {
       // const element = array[index];
 
-      const blockData = await getBlockUsingNumber("0x1");
+      if (latestblockNumber - index >= 0) {
+        await getBlockUsingNumber(
+          (latestblockNumber - index).toString(16)
+        ).then((response) => {
+          // console.log(response);
 
-      console.log(blockData);
-      console.log("get block using number");
+          emptyblocks.push(response);
+          console.log(emptyblocks);
+          setBlocks(emptyblocks);
+        });
 
+        // emptyblocks.push(blockData);
+        // console.log(emptyblocks);
+        // console.log("blocks count", emptyblocks.length);
+      }
 
-      
+      if (index == staticBlockCount - 1) {
+        // setBlocks(emptyblocks);
+      }
     }
+
+    return emptyblocks;
   }
 
   async function getBlockUsingNumber(number) {
+    // console.log("Given block number is" + "0x" + number);
+    var blockData = {};
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x1", false],"id":1})
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "eth_getBlockByNumber",
+        params: ["0x" + number, false],
+        id: 1,
+      }),
     };
-    fetch('https://ethblockchain.mohammedismayi1.repl.co/', requestOptions)
-      .then(response => 
-        console.log(response.json())
-        // response.json()
-      
-      
-      
-      
-      )
-      
-        
-        
-    
+
+    await fetch("https://ismayilethtestnet.tk/", requestOptions)
+      .then((response) => response.json())
+      .then((returnedJSON) => {
+        // console.log(returnedJSON);
+
+        blockData = returnedJSON;
+        // console.log("from blocks arr", blockData["result"]["hash"]);
+      });
+
+    if (blockData["result"] != null) {
+      return blockData;
+    }
   }
-
-
-
-
 
   function ListItem(props) {
     const value = props.value;
+    const blockNumber = parseInt(props.blockNumber, 16);
     return (
       <li>
-        {value}
+        <div>
+          {blockNumber}-{value}
+        </div>
       </li>
     );
   }
-  const listItems = blocks.map((number) =>
-  // Correct! Key should be specified inside the array.
-  <ListItem key={number.toString()} value={number} />
-);
+  const listItems =
+    blocks.length > 0 ? (
+      blocks.map((number) => (
+        // Correct! Key should be specified inside the array.
+        <ListItem
+          key={number["result"]["hash"]}
+          value={number["result"]["miner"]}
+          blockNumber={number["result"]["number"]}
+        />
+      ))
+    ) : (
+      <div>No list</div>
+    );
   return (
-    console.log("Rendering the APP"),
-    <div>ETH Explorer
-
+    // console.log("Rendering the APP"),
+    <div>
+      <div>ETH Explorer</div>
       <div>
-      <ul>
-      {listItems}
-    </ul>
-
+        <ul>{listItems}</ul>
       </div>
     </div>
-
-    
-  )
+  );
 }
